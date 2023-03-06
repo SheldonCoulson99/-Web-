cd86335ca83240374f75184f8f82cf1a5bd3bb9d
Sheldon Coulson <sheldoncoulson99@outlook.com>
2023-03-06 17:29:03 +0800

    remove idea's build 'out' folders

--- Booking/out/artifacts/Booking_war_exploded/WEB-INF/classes/com/sheldon/dao/AircraftDao.java
+++
@@ -1,9 +0,0 @@
-package com.sheldon.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sheldon.domain.Aircraft;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AircraftDao extends BaseMapper<Aircraft> {
-}
